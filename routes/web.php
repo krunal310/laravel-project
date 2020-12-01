c5f3bac526ce4130ab8829fb5060c08b3d6b7ead
Krunal Vasoya <krunalvasoya@gmail.com>
2020-12-01 21:03:52 +0530

    minor bug fixes

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\PostController::class, 'index'])->name('home');
 Route::get('posts/create', [App\Http\Controllers\PostController::class, 'create']);
-Route::post('posts', [App\Http\Controllers\PostController::class, 'store']);
-Route::get('posts/{posts}/edit', [App\Http\Controllers\PostController::class, 'edit']);
-Route::get('posts/{posts}', [App\Http\Controllers\PostController::class, 'show']);
-Route::put('posts/{posts}', [App\Http\Controllers\PostController::class, 'update']);
-Route::delete('posts/{posts}', [App\Http\Controllers\PostController::class, 'destroy']);
+Route::post('post', [App\Http\Controllers\PostController::class, 'store']);
+Route::get('posts/{post}/edit', [App\Http\Controllers\PostController::class, 'edit']);
+Route::get('posts/{post}', [App\Http\Controllers\PostController::class, 'show']);
+Route::put('posts/{post}', [App\Http\Controllers\PostController::class, 'update']);
+Route::delete('posts/{post}', [App\Http\Controllers\PostController::class, 'destroy']);
